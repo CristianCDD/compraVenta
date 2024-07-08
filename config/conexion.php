3e60f2eaea0ece60f2f2593b5fdf01a6b907ab61
Cristian <cristian10damian@gmail.com>
2024-07-08 13:00:36 -0500

    Conexion exitosa

--- config/conexion.php
+++ config/conexion.php
@@ -1,16 +1,24 @@
 <?php 
-    class Conectar{
-        protected $dbh;
+class Conectar{
+    protected $dbh;
 
-        protected function Conexion(){
-            try{
-                $conectar = $this->dbh = new PDO("sqlsrv:Server=DESKTOP-6221JPI;Database=CompraVenta", null, null);
-                return $conectar;
-            }catch(Exception $e){
-                print "Error conexion BD" . $e->getMessage() . "<br/>";
-                die();
-            }
+    public function Conexion(){
+        try{
+            $conectar = $this->dbh = new PDO("sqlsrv:Server=localhost;Database=CompraVenta", null, null);
+            return $conectar;
+        }catch(Exception $e){
+            print "Error conexion BD: " . $e->getMessage() . "<br/>";
+            die();
         }
     }
+}
 
+$conexionP = new Conectar();
+$db = $conexionP->Conexion();
+
+if($db){
+    echo "Conexion exitosa";
+}else{
+    echo "Mala conexion";
+}
 ?>
