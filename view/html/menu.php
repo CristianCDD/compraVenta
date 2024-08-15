f8b3031366e2b70f90895f47a324ac57e79697ee
Cristian <cristian10damian@gmail.com>
2024-08-15 12:36:19 -0500

    Menu, perfil

--- view/html/menu.php
+++ view/html/menu.php
@@ -1,109 +1,116 @@
-  <!-- ========== App Menu ========== -->
-  <div class="app-menu navbar-menu">
-      <!-- LOGO -->
-      <div class="navbar-brand-box">
-          <!-- Dark Logo-->
-          <a href="index.html" class="logo logo-dark">
-              <span class="logo-sm">
-                  <img src="../../assets/images/logo-sm.png" alt="" height="22">
-              </span>
-              <span class="logo-lg">
-                  <img src="../../assets/images/logo-dark.png" alt="" height="17">
-              </span>
-          </a>
-          <!-- Light Logo-->
-          <a href="index.html" class="logo logo-light">
-              <span class="logo-sm">
-                  <img src="../../assets/images/logo-sm.png" alt="" height="22">
-              </span>
-              <span class="logo-lg">
-                  <img src="../../assets/images/logo-light.png" alt="" height="17">
-              </span>
-          </a>
-          <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
-              <i class="ri-record-circle-line"></i>
-          </button>
-      </div>
+<?php
+require_once("../../models/Menu.php");
+$menu = new Menu();
+$datos = $menu->get_menu_x_rol_id($_SESSION["ROL_ID"]);
+?>
 
-      <div id="scrollbar">
-          <div class="container-fluid">
+<div class="app-menu navbar-menu">
 
-              <div id="two-column-menu">
-              </div>
-              <ul class="navbar-nav" id="navbar-nav">
-                  <li class="menu-title"><span data-key="t-menu">Menu</span></li>
+    <div class="navbar-brand-box">
 
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../home/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Dashboard</span>
-                      </a>
-                  </li>
+        <a href="index.html" class="logo logo-dark">
+            <span class="logo-sm">
+                <img src="../../assets/images/logo-sm.png" alt="" height="22">
+            </span>
+            <span class="logo-lg">
+                <img src="../../assets/images/logo-dark.png" alt="" height="17">
+            </span>
+        </a>
 
-                  <li class="menu-title"><span data-key="t-menu">Mantenimiento</span></li>
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../MntCategoria/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Mnt. categoria</span>
-                      </a>
-                  </li>
+        <a href="index.html" class="logo logo-light">
+            <span class="logo-sm">
+                <img src="../../assets/images/logo-sm.png" alt="" height="22">
+            </span>
+            <span class="logo-lg">
+                <img src="../../assets/images/logo-light.png" alt="" height="17">
+            </span>
+        </a>
 
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../MntProducto/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Mnt. producto</span>
-                      </a>
-                  </li>
+        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
+            <i class="ri-record-circle-line"></i>
+        </button>
 
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../MntCliente/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Mnt. cliente</span>
-                      </a>
-                  </li>
+    </div>
 
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../MntProveedor/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Mnt. proveedor</span>
-                      </a>
-                  </li>
+    <div id="scrollbar">
 
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../MntMoneda/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Mnt. moneda</span>
-                      </a>
-                  </li>
+        <div class="container-fluid">
 
+            <div id="two-column-menu">
+            </div>
+            <ul class="navbar-nav" id="navbar-nav">
+                <li class="menu-title"><span data-key="t-menu">Menu</span></li>
 
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../MntUndMedida/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Mnt. undMedida</span>
-                      </a>
-                  </li>
+                <?php
+                foreach ($datos as $row) {
+                    if ($row["MEN_GRUPO"] == "Dashboard" && $row["MEND_PERMI"] == "Si") {
+                ?>
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="<?php echo $row["MEN_RUTA"]; ?>">
+                                <i class="ri-honour-line"></i> <span data-key="t-widgets"><?php echo $row["MEN_NOM"]; ?></span>
+                            </a>
+                        </li>
+                <?php
+                    }
+                }
+                ?>
 
+                <li class="menu-title"><span data-key="t-menu">Mantenimiento</span></li>
 
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../MntEmpresa/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Mnt. empresa</span>
-                      </a>
-                  </li>
+                <?php
+                foreach ($datos as $row) {
+                    if ($row["MEN_GRUPO"] == "Mantenimiento" && $row["MEND_PERMI"] == "Si") {
+                ?>
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="<?php echo $row["MEN_RUTA"]; ?>">
+                                <i class="ri-honour-line"></i> <span data-key="t-widgets"><?php echo $row["MEN_NOM"]; ?></span>
+                            </a>
+                        </li>
+                <?php
+                    }
+                }
+                ?>
 
-                  <li class="nav-item">
-                      <a class="nav-link menu-link" href="../MntSucursal/">
-                          <i class="ri-dashboard-2-line"></i> <span data-key="t-widgets">Mnt. sucursal</span>
-                      </a>
-                  </li>
+                <li class="menu-title"><span data-key="t-menu">Compra</span></li>
 
-                  <li class="menu-title"><span data-key="t-menu">Compra</span></li>
+                <?php
+                foreach ($datos as $row) {
+                    if ($row["MEN_GRUPO"] == "Compra" && $row["MEND_PERMI"] == "Si") {
+                ?>
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="<?php echo $row["MEN_RUTA"]; ?>">
+                                <i class="ri-honour-line"></i> <span data-key="t-widgets"><?php echo $row["MEN_NOM"]; ?></span>
+                            </a>
+                        </li>
+                <?php
+                    }
+                }
+                ?>
 
 
-                  <li class="menu-title"><span data-key="t-menu">Venta</span></li>
+                <li class="menu-title"><span data-key="t-menu">Venta</span></li>
 
-              </ul>
-          </div>
-          <!-- Sidebar -->
-      </div>
+                <?php
+                foreach ($datos as $row) {
+                    if ($row["MEN_GRUPO"] == "Venta" && $row["MEND_PERMI"] == "Si") {
+                ?>
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="<?php echo $row["MEN_RUTA"]; ?>">
+                                <i class="ri-honour-line"></i> <span data-key="t-widgets"><?php echo $row["MEN_NOM"]; ?></span>
+                            </a>
+                        </li>
+                <?php
+                    }
+                }
+                ?>
 
-      <div class="sidebar-background"></div>
-  </div>
 
+            </ul>
+        </div>
 
-  <!-- Left Sidebar End -->
-  <!-- Vertical Overlay-->
-  <div class="vertical-overlay"></div>
+    </div>
+
+    <div class="sidebar-background"></div>
+</div>
+
+<div class="vertical-overlay"></div>
